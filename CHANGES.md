2a70446a204ce81b12c0f1fffe2781a6bc75545e
Lisias <me@lisias.net>
2021-04-13 19:32:46 -0300

    Release 2.1.5.2

--- CHANGES.md
+++ CHANGES.md
@@ -1,7 +1,9 @@
 # Animated Attachment :: Changes
 
-* 2021-0329: 2.1.5.1 (LisiasT) for KSP >= 1.4.0
+* 2021-0413: 2.1.5.2 (LisiasT) for KSP >= 1.4.0
 	+ Added KSPe facilities:
 		- Logging/Debugging
 		- File System Abstraction
 	+ Works on every KSP version from 1.4.0 to the latest.
+* 2021-0329: 2.1.5.1 (LisiasT) for KSP >= 1.4.0
+	+ ***DITCHED*** due wrongly compilation against a beta release of KSPe.
--- Source/AnimatedAttachment/Properties/AssemblyInfo.cs
+++ Source/AnimatedAttachment/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Animated Attachment")]
+[assembly: AssemblyTitle("Animated Attachment /L Unofficial")]
 [assembly: AssemblyDescription("Allows generic animations to move attached parts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("L Aerospace/KSP Division")]
