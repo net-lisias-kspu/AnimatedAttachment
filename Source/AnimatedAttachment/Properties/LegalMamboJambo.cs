53c8f089132ab12324d6e7772e645303c8d10178
Lisias T <me@lisias.net>
2021-10-30 11:17:36 -0300

    Legal mambo jambo

--- Source/AnimatedAttachment/Properties/LegalMamboJambo.cs
+++ Source/AnimatedAttachment/Properties/LegalMamboJambo.cs
@@ -3,7 +3,7 @@ namespace AnimatedAttachment_NS
 {
 	public static class LegalMamboJambo
 	{
-		public const string Company = "L Aerospace KSP Division";
+		public const string Company = "/L Aerospace KSP Division";
 		public const string Copyright = "© 2021 LisiasT";
 		public const string Product = "Animated Attachments";
 		public const string Trademark = "Animated Attachments™ by Katten, /L Unleashed™ by LisiasT";
